a6fc5dd0b058c3d02549c6e7f637708e3a3f3d0a
Beelzabee <beelzabee@gmail.com>
2018-05-06 13:27:53 -0400

    Done for now
    I need to study more

--- Assets/Scripts/Managers/Manager_Builder.cs
+++ Assets/Scripts/Managers/Manager_Builder.cs
@@ -2,6 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager_Builder : MonoBehaviour {
+public class Manager_Builder {
 
 }
--- Assets/Scripts/Managers/Manager_Input.cs
+++ Assets/Scripts/Managers/Manager_Input.cs
@@ -15,9 +15,9 @@ public class Manager_Input : MonoBehaviour {
 		// Left Click
 		if (Input.GetMouseButtonUp (0)) {
 			// If over a UI element, bail out
-//			if (EventSystem.current.IsPointerOverGameObject ()) {
-//				return;
-//			}
+			if (EventSystem.current.IsPointerOverGameObject ()) {
+				return;
+			}
 			// FIXME: Places a floor down under the mouse cursor
 			if (mode_Build_Floor) {
 				Debug.Log ("Build a floor");
